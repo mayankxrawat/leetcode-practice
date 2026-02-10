28b7f64e9e69d4d1d0170d79d0a680e3b258cbb9
Mayank Rawat <190170178+mayankxrawat@users.noreply.github.com>
2026-02-10 21:38:11 +0530

    Time: 295 ms (88.14%) | Memory: 47 MB (68.61%) - LeetSync

--- 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.java
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.java
@@ -1,7 +1,7 @@
 class Solution {
     public int longestBalanced(int[] nums) {
         int n = nums.length;
-       int maxLen = 0;
+       int maxlen = 0;
 
         for(int i = 0; i < n; i++) {
             HashSet<Integer> seteven = new HashSet<>();
@@ -16,10 +16,10 @@ class Solution {
                 }
 
                 if(seteven.size() == setodd.size()) {
-                    maxLen = Math.max(maxLen, j - i + 1);
+                    maxlen = Math.max(maxlen, j - i + 1);
                 }
             }
         }
-        return maxLen;
+        return maxlen;
     }
 }
