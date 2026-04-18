455b93d1397bf1cc0ec87bdc6b5230293ee82343
Mayank Rawat <190170178+mayankxrawat@users.noreply.github.com>
2026-04-18 17:53:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (39.89%) - LeetSync

--- 292-nim-game/nim-game.java
+++ 292-nim-game/nim-game.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean canWinNim(int n) {
-        if(n%4==0){
-            return false;
-        }
-       return true; 
+        if(n % 4 != 0 ){
+            return true;}
+       return false;
     }
 }
