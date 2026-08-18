dbac7d87523f90ec7231c993ed1eb96250260760
Mayank Rawat <190170178+mayankxrawat@users.noreply.github.com>
2026-08-18 23:01:58 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (90.18%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -13,28 +13,17 @@ class Solution {
         if( head==null || head.next==null)
         return head;
 
-        List<Integer> values = new ArrayList<>();
-        ListNode curr = head;
-        while(curr!=null){
-            values.add(curr.val);
-            curr = curr.next;
-        }
+       ListNode odd = head;
+       ListNode even = head.next;
+       ListNode evenhead = even;
 
-            List<Integer> grouped = new ArrayList<>();
-        for(int i = 0 ; i<values.size(); i+=2)
-        grouped.add(values.get(i));
-            
-          for(int i = 1 ; i<values.size(); i+=2)
-        grouped.add(values.get(i));
-
-
-        ListNode dummy = new ListNode(-1);
-        curr = dummy;
-        for(int val : grouped){
-            curr.next = new ListNode(val);
-            curr=curr.next;
-        }
-
-        return dummy.next;
+       while(even != null && even.next != null){
+        odd.next = even.next;
+        odd =odd.next;
+        even.next = odd.next;
+        even=even.next;
+       }
+       odd.next=evenhead;
+       return head;
     }
 }
