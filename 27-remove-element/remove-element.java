c85fc49119686f4053298122fe2b062d78c20c23
Mayank Rawat <190170178+mayankxrawat@users.noreply.github.com>
2026-09-10 03:12:26 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (8.85%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -4,7 +4,7 @@ class Solution {
         int i=0;
         while(i<n){
             if(nums[i]==val){
-                nums[i]= nums[n-1];
+                nums[i] = nums[n-1];
                 n--;
             }else{
                 i++;
